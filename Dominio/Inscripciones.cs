2aebd9cecac6b8171ee6f010ee171b4ccf5af8d9
farguello <fernandoarguello83@gmail.com>
2019-05-05 17:23:11 -0400

    Relación de la Tabla Inscripciones con las tablas Alumno, MallaAlumnos, Matricula, Horarios y Exámenes.

--- Dominio/Inscripciones.cs
+++ Dominio/Inscripciones.cs
@@ -18,17 +18,21 @@ namespace Dominio
         [Required]
         [Display(Name = "Id de Matricula")]
         public int IdMatricula { get; set; }
+        public virtual Alumno Alumno { get; set; }
         [Required]
         [Display(Name = "Id de Malla")]
         public int IdMallaAlum { get; set; }
+        public virtual MallaAlumnos MallaAlumnos { get; set; }
         [Required]
         [Display(Name = "Id de Horario")]
         public int IdHorario { get; set; }
+        public virtual Horarios Horarios { get; set; }
         [Required]
         [Display(Name = "Fecha de Inscripción")]
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime Fecha { get; set; }
-        
+
+        public virtual ICollection<Examenes> Examenes { get; set; }
     }
 }
