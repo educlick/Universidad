083515f5bdeb0734973e9327850fee644984e4a5
farguello <fernandoarguello83@gmail.com>
2019-05-08 20:05:09 -0400

    Claves en tabla Matricula

--- Dominio/Matricula.cs
+++ Dominio/Matricula.cs
@@ -10,9 +10,19 @@ namespace Dominio
     public class Matricula
     {
         [Key]
+        [Display(Name = "Id de Matricula")]
         public int IdMatricula { get; set; }
+        [Required]
+        [Display(Name = "Id de Carrera")]
         public int IdCarrera { get; set; }
+        public virtual Carreras Carreras { get; set; }
+        [Required]
+        [Display(Name = "Id de Alumno")]
         public int IdAlumno { get; set; }
+        public virtual Alumno Alumno { get; set; }
+        [Display(Name = "Fecha de Matricula")]
         public DateTime Fecha { get; set; }
+
+        public virtual ICollection<Inscripciones> Inscripciones { get; set; }
     }
 }
