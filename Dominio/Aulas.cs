46f552504707c19b64c945b138982ace42150401
farguello <fernandoarguello83@gmail.com>
2019-05-08 20:17:31 -0400

    Se crea la tabla Aula.

---
+++ Dominio/Aulas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Aulas
+    {
+        [Key]
+        [Display(Name = "Id de Aula")]
+        public int IdAula { get; set; }
+        [Required]
+        [Display(Name = "Id de Tipo de Aula")]
+        public int IdTipoAula { get; set; }
+        public virtual TipoAula TipoAula { get; set; }
+        [Required]
+        [Display(Name = "Descripción")]
+        public char Descripcion { get; set; }
+        [Display(Name = "Edificio")]
+        public char Edificio { get; set; }
+        [Display(Name = "Piso")]
+        public char Piso { get; set; }
+        [Display(Name = "Georeferencia")]
+        public char georeferencia { get; set; }
+
+        public virtual ICollection<HorarioExamen> HorarioExamen { get; set; }
+        public virtual ICollection<Horarios> Horarios { get; set; }
+
+    }
+}
