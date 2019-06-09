05c1d4540a5221a0306007968def4c3b014422c3
farguello <fernandoarguello83@gmail.com>
2019-06-09 14:19:14 -0400

    Ajustes a la tabla Sexo

--- Dominio/Sexo.cs
+++ Dominio/Sexo.cs
@@ -13,8 +13,10 @@ namespace Dominio
         public int IdSexo { get; set; }
 
         [Required]
+        [Display(Name = "Descripción")]
         [StringLength(30, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}", MinimumLength = 2)]
         public string Descripcion { get; set; }
         public virtual ICollection <Alumno> Alumno { get; set; }
+        public virtual ICollection <Profesores> Profesores { get; set; }
     }
 }
