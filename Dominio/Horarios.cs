0cb3f7595dfbb53d3c947fd0e33a1f01e8b10b2e
farguello <fernandoarguello83@gmail.com>
2019-06-09 12:36:41 -0400

    Relaciones en Tabla Horarios

--- Dominio/Horarios.cs
+++ Dominio/Horarios.cs
@@ -12,11 +12,15 @@ namespace Dominio
         [Key]
         public int IdHorario { get; set; }
         public int IdAula { get; set; }
+        public virtual Aulas Aulas { get; set; }
         public int IdProfesor { get; set; }
+        public virtual Profesores Profesores { get; set; }
         public int IdMateria { get; set; }
+        public virtual Materias Materias { get; set; }
         public int IdTurno { get; set; }
         public virtual Turnos Turnos { get; set; }
         public int Cupo { get; set; }
 
+        public virtual ICollection<Inscripciones> Inscripciones { get; set; }
     }
 }
