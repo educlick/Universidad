7d39c9564e3b0e6054f325acc631cd96606145bb
farguello <fernandoarguello83@gmail.com>
2019-06-09 14:12:44 -0400

    Se agregan relaciones en la tabla Ciudad

--- Dominio/Ciudad.cs
+++ Dominio/Ciudad.cs
@@ -13,9 +13,11 @@ namespace Dominio
         public int IdCiudad { get; set; }
 
         [Required]
+        [Display(Name = "Descripción")]
         [StringLength(30, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}", MinimumLength = 2)]
         public string Descripcion { get; set; }
         public virtual ICollection <Alumno> Alumno { get; set; }
+        public virtual ICollection <Profesores> Profesores { get; set; }
 
     }
 }
